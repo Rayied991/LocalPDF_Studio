df159fbb6b0d2521838ac4a6b7ca3fa7527cb17c
Md. Alinur Hossain <alinur1160@gmail.com>
2025-12-06 19:22:26 +0600

    Changed the zoom scale for PDF reader from 1.0 to 1.25

--- src/pdf/web/viewer.mjs
+++ src/pdf/web/viewer.mjs
@@ -110,7 +110,7 @@ const {
 
 ;// ./web/ui_utils.js
 
-const DEFAULT_SCALE_VALUE = "1.0";
+const DEFAULT_SCALE_VALUE = "1.25";
 const DEFAULT_SCALE = 1.0;
 const DEFAULT_SCALE_DELTA = 1.1;
 const MIN_SCALE = 0.1;
